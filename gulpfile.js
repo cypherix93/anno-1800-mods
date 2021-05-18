28aec3f94571404c0e9d6703ab9859c7ab42d092
cypherix93 <bikram.ghosh93@gmail.com>
2021-05-18 00:40:44 -0400

    gulpfile update

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
-const gulp = require('gulp');
-const ejs = require('gulp-ejs');
-const rename = require('gulp-rename');
+const gulp = require('gulp')
+const ejs = require('gulp-ejs')
+const rename = require('gulp-rename')
 
-exports.build = function () {
+function compile() {
     return gulp.src('./src/*/index.ejs')
         .pipe(ejs())
         .pipe(rename('[Gameplay] All Production Buildings Unlocked in Old World/data/config/export/main/asset/assets.xml'))
-        .pipe(gulp.dest('./dist'));
-};
+        .pipe(gulp.dest('./dist'))
+}
 
-exports.default = exports.build;
+exports.default = compile
